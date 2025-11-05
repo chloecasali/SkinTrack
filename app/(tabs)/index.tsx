2e7a8e0d3aa4f2df98c05017e195553eb1544126
Chloé Casali <chlooe.casali@gmail.com>
2025-11-05 17:49:23 +0100

    wip auth process to redirect on / or on /auth/login if user is connected

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,7 @@
-import { View, Text } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+  const isAuthenticated = false;
+
+  return <Redirect href={isAuthenticated ? "/" : "/auth/login"} />;
 }
