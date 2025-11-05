2920f881e0d36b2115477a666d363150aa9db51e
Chloé Casali <chlooe.casali@gmail.com>
2025-11-05 17:09:13 +0100

    make prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
-import {Animated} from 'react-native';
-import "../../global.css"
+import { Animated } from "react-native";
+import "../../global.css";
 import View = Animated.View;
 import Text = Animated.Text;
 
 export default function HomeScreen() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-          <Text className="text-xl font-bold text-blue-500">
-              Welcome to Nativewind!
-          </Text>
-      </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
   );
 }
