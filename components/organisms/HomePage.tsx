e666926491d1c47ced19f528d8e4136c7bd9aa64
Chloé Casali <chlooe.casali@gmail.com>
2026-02-02 11:23:01 +0100

    fix prettier

--- components/organisms/HomePage.tsx
+++ components/organisms/HomePage.tsx
@@ -1,27 +1,27 @@
-import {useRouter} from "expo-router";
-import {useProfile} from "@/hooks/auth/useProfile";
-import {Text, View} from "react-native";
+import { useRouter } from "expo-router";
+import { useProfile } from "@/hooks/auth/useProfile";
+import { Text, View } from "react-native";
 import PrimaryButton from "@/components/atoms/PrimaryButton";
-import {clearToken} from "@/lib/token";
-import {APP_AUTH_LOGIN} from "@/constants/app";
+import { APP_AUTH_LOGIN } from "@/constants/app";
+import { clearToken } from "@/services/auth/token";
 
 export default function HomeScreen() {
-    const router = useRouter();
-    const { firstname } = useProfile();
+  const router = useRouter();
+  const { firstname } = useProfile();
 
-    return (
-        <View className="flex-1 items-center justify-center bg-white px-6">
-            <Text className="text-3xl font-bold mb-6">
-                Hello {firstname || "Guest"}!
-            </Text>
+  return (
+    <View className="flex-1 items-center justify-center bg-white px-6">
+      <Text className="text-3xl font-bold mb-6">
+        Hello {firstname || "Guest"}!
+      </Text>
 
-            <PrimaryButton
-                title="Disconnect"
-                onPress={() => {
-                    clearToken();
-                    router.replace(APP_AUTH_LOGIN);
-                }}
-            />
-        </View>
-    );
+      <PrimaryButton
+        title="Disconnect"
+        onPress={() => {
+          clearToken();
+          router.replace(APP_AUTH_LOGIN);
+        }}
+      />
+    </View>
+  );
 }
